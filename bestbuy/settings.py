aa317786833c6873bf2c9e7b6c8f86faa4431c0f
Timur <htimur509@gmail.com>
2025-06-25 16:59:05 +0500

    staticfiles problem

--- bestbuy/settings.py
+++ bestbuy/settings.py
@@ -138,6 +138,7 @@ USE_TZ = False
 
 # Static
 STATIC_URL = '/static/'
+
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
