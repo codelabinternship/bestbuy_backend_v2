3f68d619c3579c64b614b8a80c7fa65547297ca2
Timur <htimur509@gmail.com>
2025-06-26 16:10:27 +0500

    CORS error fix

--- bestbuy/settings_prod.py
+++ bestbuy/settings_prod.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,21 +53,24 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware'
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "http://localhost:5173",
-]
-CSRF_TRUSTED_ORIGINS=[
-    "http://localhost:3000",
-    "http://localhost:3001",
-]
+#CORS_ALLOWED_ORIGINS = [
+#    "http://localhost:3000",
+#    "http://localhost:3001",
+ #   "http://localhost:5173",
+#]
 
+CORS_ALLOW_ALL_ORIGINS = True
 
+#CSRF_TRUSTED_ORIGINS=[
+ #   "http://localhost:3000",
+  #  "http://localhost:3001",
+#]
+
+CORS_ALLOW_CREDENTIALS
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
